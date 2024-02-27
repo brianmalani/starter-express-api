f99fd42758c3df85043e583bef9affc64899e00c
brianmalani <collectivecodecrafter@gmail.com>
2024-02-27 10:58:33 +0200

    editing user.js models

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-const User = require('../models/User');
+const User = require('../models/user');
 
 router.get('/', async (req, res) =>{
     const userList = await User.find().select('-passwordHash');
